1e0a5b8dfb513a6ddb7596783353542672de09d0
= <=>
2015-02-17 15:26:37 +0530

    wrote test for getting words in a string and getting number of lines in a string

--- Wc.java
+++ Wc.java
@@ -7,4 +7,14 @@ public class Wc{
 	public int getLength(){
 		return str.length();
 	}
+
+	public int getWords(){
+		String[] s1 = str.split(" ");
+		return s1.length;
+	}
+
+	public int getLine(){
+		String[] s1 = str.split("\r\n");
+		return s1.length;
+	}
 }
--- WcTest.java
+++ WcTest.java
@@ -7,4 +7,25 @@ public class WcTest{
 		Wc w = new Wc("hello");
 		assertEquals(5,w.getLength());
 	}
+
+	@Test
+	public void itCanGiveLengthOfStringWithSpaces(){
+		String s1 = "India is my country";
+		Wc w = new Wc(s1);
+		assertEquals(19,w.getLength());
+	}
+
+	@Test
+	public void canGiveNoOfWordsInAString(){
+		String s1 = "India is my country";
+		Wc w = new Wc(s1);
+		assertEquals(4,w.getWords());
+	}
+
+	@Test
+	public void canGiveNoOflinesInString(){
+		String s1 = "India is my country";
+		Wc w = new Wc(s1);
+		assertEquals(1,w.getLine());
+	}
 }
